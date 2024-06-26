f0c054f53b2e060b4a199e5469df46df0fff3b68
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-26 17:51:54 +0200

    snack completed

--- snack4/index_4.php
+++ snack4/index_4.php
@@ -1,1 +1,15 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+
+<?php
+    $casualNums = [];
+
+
+    for ($i = 0; $i <= 15; $i++) {
+                $num = rand(1, 100);
+    if (!in_array($num, $casualNums)){
+        array_push($casualNums, $num);
+    }}
+
+    var_dump($casualNums);
+?>
