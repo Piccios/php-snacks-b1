714be45ecb95b6dfc9c5da68f7bbe1eb5a127045
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-26 12:22:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+
+
+
+<?php
+
+$games = [
+    [
+        'HomeTeam' => 'Nuovo Basket Altopascio',
+        'AwayTeam' => 'Lucchese Basket',
+        'HomeScore' => '80',
+        'AwayScore' => '50'
+    ],
+    [
+        'HomeTeam' => 'Pisa Basket',
+        
+    ]
+    ];
+    
