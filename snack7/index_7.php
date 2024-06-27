cd98f449c38dae9dd82287276725853a0f15abda
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-27 12:32:08 +0200

    snack completed

--- snack7/index_7.php
+++ snack7/index_7.php
@@ -1,3 +1,18 @@
-Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+<!-- Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
 Questo array dovra' essere inserito in un file separato, da importare.
-Stampare Nome, Cognome e la media dei voti di ogni alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+
+include __DIR__ . '/students.php';
+
+foreach ($students as $student){
+    $media = array_sum($student['votes']) / count($student['votes']);
+    echo "Nome: " . $student['name'] . "<br>";
+    echo "Cognome: " . $student['lastname'] . "<br>";
+    echo "Media: " . $media . "<br>";
+}
+
+
+
+?>
---
+++ snack7/students.php
@@ -0,0 +1,33 @@
+<?php
+// include __DIR__ . '/index_7.php';
+    $students = [
+        [
+            'name' => 'Marco',
+            'lastname' => 'Rossi',
+            'votes' => [
+                6,
+                8,
+                9
+            ]
+        ],
+        [
+            'name' => 'Giuseppe',
+            'lastname' => 'Verdi',
+            'votes' => [
+                2,
+                8,
+                5
+            ]
+        ],
+        [
+            'name' => 'Mario',
+            'lastname' => 'Bianchi',
+            'votes' => [
+                7,
+                8,
+                8
+            ]
+        ]
+    ];
+
+?>
