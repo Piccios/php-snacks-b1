6483f3cb3c4a1dac082df7e07376d57139d008f5
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-27 02:00:48 +0200

    snack test

--- snack6/index_6.php
+++ snack6/index_6.php
@@ -1,1 +1,49 @@
 <!-- Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+
+<?php
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-6</title>
+</head>
+<body>
+    <body>
+        <div class="teachers">
+            
+        </div>
+        <div class="pm">
+
+        </div>
+    </body>
+</body>
+</html>
